7d5b893d6ff89f5dbfaeff7993d77edc05b6bb99
Jacobjp32 <jacobjonson32@gmail.com>
2026-07-10 14:10:43 -0300

    feat(home): prioriza eventos unicos na grade Acontece em breve

--- js/site-meta.js
+++ js/site-meta.js
@@ -3,7 +3,7 @@
 
   window.SITE_META = window.SITE_META || {
     version: "Turismo SMS 1.0",
-    updatedAt: "2026-07-09T13:33:24-03:00",
+    updatedAt: "2026-07-10T14:09:18-03:00",
     environment: "production"
   };
 
